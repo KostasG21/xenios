c51c0479e75d6cdf64cfc4cfae5be26fca8196f5
costas <kgongas@gmail.com>
2021-04-20 18:24:17 -0500

    actualizacion logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -190,7 +190,7 @@ latex_elements = {
             \vspace{0mm}
             \begin{figure}[!h]
                 \centering
-                \includegraphics[scale=0.3]{foqus_logo_metal.png}
+                \includegraphics[scale=0.3]{logo_xenios.png}
             \end{figure}
 
             \vspace{0mm}
